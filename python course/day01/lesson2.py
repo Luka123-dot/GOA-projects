c26db64f9649cfb747537e721ca04b44ec2f51d8
Luka123-dot <l_ka3@Icloud.com>
2025-01-12 10:40:23 -0800

    Add files via upload

---
+++ python course/day01/lesson2.py
@@ -0,0 +1,41 @@
+name = "Luka"
+#name არის ცვლადი
+# = არის ცვლადისტვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "luka" არის ცვლადისტვის ,მნიშვნელობა
+
+surname = "Sadunishvili"
+sigma = "is"
+print(name)
+#პრინტ ფუნციას გადაეცემა ეკრანზე გამოსატანი ობიეკტი
+
+ნამე = "ლუკა" #ეს არის str (string) ტაპის ცვლადი
+age = " age 12" # ეს არის int (integer) მთელი რიცხვი
+height = " 5 feet," #ეს არის float ტიპის ცვლადი  ( ათწილადი ) 
+#boolean (bool) ტიპის ცვლადი
+
+Just_started_programming = True    #True ან False 
+is_ugly = False     #snakecase( უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False # ჯავასკრიპტული camelcase
+
+skibidi = "whose height is"
+cool = "he is from"
+# print(name + " " + surname)
+chad = "georgia"
+# სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+# print (name + age)
+seargent = "and he just started programming."
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(Just_started_programming))
+
+print(name + " " + str(age))
+
+print((name) + " " + (surname) + str(age) + str(height))
+
+print((name) + " " + (surname) + " " + str(age) + " " + str(height))
+# print("i am (name) + " " + (surname) and i am + " " + str(age) and i am + " " + str(age) ")
+
+print((name) + " " + (surname) + " " + str(sigma) + " " + str(age) + " " + str(skibidi) + " " + str(height) + " " + (cool) + " " + (chad) + " " + (seargent))
